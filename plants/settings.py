d6b6fd52b066559030e0e88a078be9f7f03fe12a
aruviii <usedarktheme@gmail.com>
2021-07-12 16:34:34 +0530

    first commit

--- plants/settings.py
+++ plants/settings.py
@@ -120,7 +120,7 @@ CELERY_BROKER_URL = 'redis://localhost:6379'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
